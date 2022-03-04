d7901df971e0e40e4ecf7cc99d619c56ae570b02
unknown <ZAKARIA.CHOUKRI@um6p.ma>
2022-03-04 10:24:44 +0100

    changes

--- main.py
+++ main.py
@@ -89,19 +89,21 @@ class Student(Person):
         self.course_grades = {i: {"TP": None, "DS": None, "Project": None, "TD": None, "final": None} for i in enrolled_courses}
         self.final_grades = {i: None for i in enrolled_courses}
 
-
     def get_course_grades(self, course):
         return self.course_grades[course]
 
     def get_enrolled_courses(self):
         return self.enrolled_courses
 
+    def get_final_grades(self, course):
+        return self.final_grades[course]
 
 class Course:
     """This is the course class"""
 
 
     def __init__(self, name, Students, Professor, infos: dict = None):
+
         self.name = name
         self.Students = Students
         self.infos = infos
@@ -129,3 +131,40 @@ class Course:
                 if student.course_grades[self.name][i] != None:
                     final_grade += student.course_grades[self.name][i] * self.coeffs[i]
             student.final_grades[self.name] = final_grade
+
+    def display_final_grades(self):
+        return {i: i.final_grades[self.name] for i in self.Students}
+
+
+class Professor(Person):
+    """This is the Professor person"""
+    
+    def __init__(self, name: str, ID: int, email: str, Programs: list):
+        super().__init__(name, ID, email)
+        self.Programs = Programs
+
+    def add_program(self, program):
+        self.Programs.append(program)
+
+    def get_programs(self):
+        return self.Programs
+
+class Promo:
+    """This is the Promo class"""
+
+    def __init__(self, students: list, year: str):
+        self.year = year
+        self.students = students
+    
+    def display(self):
+        pass
+
+fri = Student("free", 71, "fri@um6p.ma", ingenior_info, [LBD, analise])
+basma = Student("bsma", 97, "bsma@um6p.ma", ingenior_info, [LBD, analise])
+assklou = Professor("Assklou", 123, "assklou@um6p.ma", [ingenior_info])
+mol_analyse = Professor("mol_analyse", 23, "analyse@um6p.ma", [ingenior_info])
+lbd = Course("lbd", [fri, basma], assklou)
+analise = Course("analyse", [fri, basma], mol_analyse)
+ingenior_info = Program("ing cs", "bngrir", "um6p-cs.ma", "0811", "cs@um6p.ma", "2021", list_of_courses, list_of_students)
+cs = School("cs", "bngrir", "um6p-cs.ma", "0811", "cs@um6p.ma", "2018", list_of_programs)
+um6p = University("um6p", "bngrir", "um6p.ma", "0811", "um6p@um6p.ma", "2013",)
